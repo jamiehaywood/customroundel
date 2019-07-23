97a9f88b3f58c488d0c47e510ca2cd3157401907
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-23 23:08:16 +0100

    implemented input parent state using react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Roundel from './Roundel';
+import InputBox from './InputBox';
+
+
 
 const App: React.FC = () => {
-
+  const [input, setInput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <Roundel />
-      </header>
+    <div>
+      <div className="roundel">
+        <Roundel text={input}/>
+      </div>
+      <div>
+        <InputBox input={input} setInput={setInput}/>
+      </div>
     </div>
   );
 }
