a46a75236990b863e7ec020b7727784c810936ea
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-27 13:32:00 +0100

    Updates

---
+++ precache-manifest.970dc1f7381df58a61087dfde2c10773.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0665a6bc83e73856a1d0cece6af6d069",
+    "url": "/customroundel/index.html"
+  },
+  {
+    "revision": "7b1a9f8b963ac7fff49a",
+    "url": "/customroundel/static/css/main.2422ba18.chunk.css"
+  },
+  {
+    "revision": "8568f50f7ceb80227548",
+    "url": "/customroundel/static/js/2.d42b6f0b.chunk.js"
+  },
+  {
+    "revision": "7b1a9f8b963ac7fff49a",
+    "url": "/customroundel/static/js/main.35495b16.chunk.js"
+  },
+  {
+    "revision": "b3440d5b791f1879ba5e",
+    "url": "/customroundel/static/js/runtime~main.f7dbaff5.js"
+  },
+  {
+    "revision": "470c2dfbeaa74eeda70b383db6cc2aa1",
+    "url": "/customroundel/static/media/Johnston100W03-Light.470c2dfb.svg"
+  },
+  {
+    "revision": "b0b5e0e86f9abb4a791d69e673fffe88",
+    "url": "/customroundel/static/media/Johnston100W03-Light.b0b5e0e8.woff2"
+  },
+  {
+    "revision": "be499e7120ddfa59344370a90a380876",
+    "url": "/customroundel/static/media/Johnston100W03-Light.be499e71.woff"
+  },
+  {
+    "revision": "dbb9726f467f87d1acf343ce98704569",
+    "url": "/customroundel/static/media/Johnston100W03-Light.dbb9726f.ttf"
+  },
+  {
+    "revision": "ef0b8e643bac5abcde656f93ab323c53",
+    "url": "/customroundel/static/media/Johnston100W03-Light.ef0b8e64.eot"
+  },
+  {
+    "revision": "18caf42645db45904fc711838d60c583",
+    "url": "/customroundel/static/media/Johnston100W03-Medium.18caf426.svg"
+  },
+  {
+    "revision": "4c6e937d68278d9ac76c72921cfdf8b8",
+    "url": "/customroundel/static/media/Johnston100W03-Medium.4c6e937d.ttf"
+  },
+  {
+    "revision": "9c9e6f5a84f1e9421626d4bb96d0314d",
+    "url": "/customroundel/static/media/Johnston100W03-Medium.9c9e6f5a.woff2"
+  },
+  {
+    "revision": "a7afe23276a8b4311238980510687c25",
+    "url": "/customroundel/static/media/Johnston100W03-Medium.a7afe232.woff"
+  },
+  {
+    "revision": "f5ee8c89414a2ae191bf981b3ee6e12c",
+    "url": "/customroundel/static/media/Johnston100W03-Medium.f5ee8c89.eot"
+  },
+  {
+    "revision": "08cbc8eb9051799bad57a836ca2135d5",
+    "url": "/customroundel/static/media/Johnston100W03-Regular.08cbc8eb.woff"
+  },
+  {
+    "revision": "0ea5a2ceffb9af477576260aeac4d3ed",
+    "url": "/customroundel/static/media/Johnston100W03-Regular.0ea5a2ce.ttf"
+  },
+  {
+    "revision": "3e4884c3f76fd92a82191cf912d10a06",
+    "url": "/customroundel/static/media/Johnston100W03-Regular.3e4884c3.svg"
+  },
+  {
+    "revision": "adf5cbb89800ccf949a7db31013b3cbe",
+    "url": "/customroundel/static/media/Johnston100W03-Regular.adf5cbb8.eot"
+  },
+  {
+    "revision": "fd0a76c7579d9c22fe1d6077a6a8bb87",
+    "url": "/customroundel/static/media/Johnston100W03-Regular.fd0a76c7.woff2"
+  }
+]);
