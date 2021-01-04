37aa5b45a0aecdba16a3cd64e98d94496efc02d4
Jamie Haywood <jamie.haywood@gmail.com>
2021-01-04 22:52:32 +0000

    refactor: small cleanup

--- src/ColorBox.tsx
+++ src/ColorBox.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import './ColorBox.scss'
+import React from "react";
+import "./ColorBox.scss";
 
 interface Props {
-    setRoundelColour: Function,
-    colour: string,
+  setRoundelColour: (colour: string) => void;
+  colour: string;
 }
 
-const ColorBox: React.FC<Props> = (props) => {
-    const colour = props.colour
+const ColorBox: React.FC<Props> = ({ colour, setRoundelColour }) => {
+  const divStyle = {
+    width: 40,
+    height: 40,
+    background: colour,
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "#707070",
+    borderRadius: 5,
+    padding: 5,
+    cursor: "pointer",
+  };
 
-    const divStyle = {
-        width: 40,
-        height: 40,
-        background: colour,
-        borderStyle: "solid",
-        borderWidth: 1,
-        borderColor: "#707070",
-        borderRadius: 5,
-        padding: 5,
-        cursor: "pointer"
-    }
+  return (
+    <div className="box">
+      <button
+        style={divStyle}
+        onClick={() => setRoundelColour(colour)}
+      ></button>
+    </div>
+  );
+};
 
-    return (
-        <div className="box">
-            <button style={divStyle} onClick={() => props.setRoundelColour(colour)}></button>
-        </div>
-    )
-}
-
-export default ColorBox
+export default ColorBox;
